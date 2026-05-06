80e3f60a1b6b849d314b211298450761f9c78dc9
Adnannewb <rahadnan6@gmail.com>
2026-05-06 08:46:01 +0600

    Strategy Pattern

--- Strategy Pattern/with_pattern.py
+++ Strategy Pattern/with_pattern.py
@@ -1,5 +1,6 @@
-from abc import ABC
+from abc import ABC,abstractmethod
 class DiscountStrategy(ABC):
+    @abstractmethod
     def calculate_discount(self):
         pass
 
@@ -13,6 +14,18 @@ class FirstOrderDiscount(DiscountStrategy):
 class DiscountService:
     def __init__(self,discount_strategy:DiscountStrategy):
         self.__discount_strategy=discount_strategy
-    def set_strategy(self):
-        pass
-        
+    
+    def set_strategy(self,new_discount_strategy:DiscountStrategy):
+        self.__discount_strategy=new_discount_strategy
+    
+    def process(self):
+        self.__discount_strategy.calculate_discount()
+
+ed=EidDiscount()
+fo=FirstOrderDiscount()
+
+ds=DiscountService(fo)
+ds.process()
+
+ds.set_strategy(ed)
+ds.process()
